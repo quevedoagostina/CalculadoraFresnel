e3543ceb8dea80f8bd210a91fa84feeeaaece088
quevedoagostina <a.quevedo@itecriocuarto.org.ar>
2024-08-13 22:03:51 -0300

    Styles better and calculation

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ document.getElementById('fresnel-form').addEventListener('submit', function(even
 
     if (distance > 0 && frequency > 0) {
         const fresnelZone = 8.656 * Math.sqrt(distance / frequency);
-        document.getElementById('fresnel-value').textContent = fresnelZone.toFixed(2);
+
+        const truncatedFresnelZone = Math.floor(fresnelZone * 100000) / 100000;
+
+        document.getElementById('fresnel-value').textContent = truncatedFresnelZone.toFixed(5);
         document.getElementById('result').style.display = 'block';
         document.getElementById('result').classList.add('show');
     } else {
@@ -21,4 +24,17 @@ document.getElementById('reset-button').addEventListener('click', function() {
     document.getElementById('frequency').value = '';
     document.getElementById('result').style.display = 'none';
     document.getElementById('result').classList.remove('show');
+});
+
+const toggleButton = document.getElementById('toggleInstructions');
+const instructions = document.getElementById('instructions');
+
+toggleButton.addEventListener('click', function() {
+    if (instructions.style.display === 'none' || instructions.style.display === '') {
+        instructions.style.display = 'block';
+        toggleButton.textContent = 'Cerrar Instrucciones';
+    } else {
+        instructions.style.display = 'none';
+        toggleButton.textContent = 'Abrir Instrucciones';
+    }
 });
